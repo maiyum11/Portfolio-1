3fad10b3ad995e3baffea89aae586084efa461c4
maiyum11 <mariam.shahnawaz@ontariotechu.net>
2022-10-29 22:51:31 -0500

    changes-11

--- controller/index.js
+++ controller/index.js
@@ -3,19 +3,19 @@ var app = express.Router();
 
 /* GET home page. */
 
-app.get('/home', function(req, res, next) {
+router.get('/home', function(req, res, next) {
   res.render(('index'), { title: 'Homepage', section: '2nd year Networking and IT security student' });
 });
 /* GET about me page. */
-app.get('/aboutme', function(req, res, next) {
+router.get('/aboutme', function(req, res, next) {
   res.render('index', { title: 'About Me' });
 });
 /* GET projects page. */
-app.get('/projects', function(req, res, next) {
+router.get('/projects', function(req, res, next) {
   res.render('index', { title: 'Projects Page' });
 });
 /* GET contact me page. */
-app.get('/contactme', function(req, res, next) {
+router.get('/contactme', function(req, res, next) {
   res.render('index', { title: 'Contact Me' });
 });
-module.exports = app;
+module.exports = router;
