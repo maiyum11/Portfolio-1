fb6172c89352e92812f54fda15a2b96267f9748c
maiyum11 <mariam.shahnawaz@ontariotechu.net>
2022-10-29 22:37:05 -0500

    changes-11

--- app.js
+++ app.js
@@ -39,7 +39,11 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error',
+  {
+    title:"Error"
+  }
+  );
 });
 
 module.exports = app;
